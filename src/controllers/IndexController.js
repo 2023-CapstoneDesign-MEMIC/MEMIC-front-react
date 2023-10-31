b567c20dc281ea88d42b404761a4a1058e6862bc
dev-joeysos <joeysos@naver.com>
2023-10-31 15:44:09 +0900

    Update sample controller(#2)

---
+++ src/controllers/IndexController.js
@@ -0,0 +1,12 @@
+//this is sample controller
+
+// import React from 'react'
+// import IndexView from '../views/IndexView'
+
+/* 리액트 동적 props 추가
+export default () => (
+    <IndexView>
+        <contact-form {..props} />
+    </IndexView>
+)
+*/
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+import './styles';
+import './scripts';
+import IndexView from './views/IndexView';
+import IndexController from './controllers/IndexController'
+import InputView from './views/InputView';
+import RecordView from './views/RecordView';
+import FeedbackView from './views/FeedbackView';
+import FaqsView from './views/FaqsView';
+import UnauthorizedView from './views/UnauthorizedView';
+import NotFoundView from './views/NotFoundView';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            //<Route path="/" element={<IndexView />} />
+            <Route path="/" element={<IndexController />} />
+            <Route path="/input.html" element={<InputView />} />
+            <Route path="/record.html" element={<RecordView />} />
+            <Route path="/feedback.html" element={<FeedbackView />} />
+            <Route path="/faqs.html" element={<FaqsView />} />
+            <Route path="/401.html" element={<UnauthorizedView />} />
+            <Route path="/404.html" element={<NotFoundView />} />
+
+        </Routes>
+    </BrowserRouter>
+, document.getElementById('root'));
+// serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
