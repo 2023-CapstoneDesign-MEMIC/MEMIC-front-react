0454bd08a374e44c633994e01802f3093dfcb6e3
dev-joeysos <joeysos@naver.com>
2023-11-01 20:43:59 +0900

    Update YIC with POST call + CSS (#7)

--- src/views/YoutubeInputComponent.js
+++ src/views/YoutubeInputComponent.js
@@ -1,67 +1,73 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import './YouTubeComponent.css'; // CSS 파일을 임포트합니다.
 
-function YouTubeInputComponent() {
-    const [url, setUrl] = useState('');
-    const [timeline, setTimeline] = useState('');
-    const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState('');
+const YouTubeComponent = () => {
+  const [link, setLink] = useState('');
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
 
-    const validateInput = () => {
-        const urlPattern = /^(http|https):\/\/(www.youtube.com|youtu.be)\/.+/;
-        const timelinePattern = /^([0-5]?[0-9]):([0-5]?[0-9])~([0-5]?[0-9]):([0-5]?[0-9])$/;
+  const handleLinkChange = (event) => {
+    setLink(event.target.value);
+  };
 
-        if (!urlPattern.test(url)) {
-            setError('Please enter a valid YouTube URL.');
-            return false;
-        }
+  const handleStartChange = (event) => {
+    setStart(event.target.value);
+  };
 
-        if (!timelinePattern.test(timeline)) {
-            setError('Please enter a valid timeline format (00:00~00:10).');
-            return false;
-        }
+  const handleEndChange = (event) => {
+    setEnd(event.target.value);
+  };
 
-        setError('');
-        return true;
-    };
+  const handleSubmit = async () => {
+    if (link && start && end) {
+      const formData = new FormData();
+      formData.append('link', link);
+      formData.append('start', start);
+      formData.append('end', end);
 
-    const handleSubmit = () => {
-        if (validateInput()) {
-            setSubmitted(true);
-        }
-    };
+      try {
+        await axios.post('http://127.0.0.1:8000/fileupload/youtube', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        alert('동영상 처리 요청을 보냈습니다.');
+      } catch (error) {
+        console.error('There was an error!', error);
+      }
+    } else {
+      alert('모든 필드를 채워주세요.');
+    }
+  };
 
-    return (
-        <div>
-            {submitted ? (
-                <p style={{ color: 'green' }}>Great! Well done, Now move on to Record page!</p>
-            ) : (
-                <div>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Enter YouTube URL"
-                            value={url}
-                            onChange={(e) => setUrl(e.target.value)}
-                            disabled={submitted}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Enter Timeline (00:00~00:10)"
-                            value={timeline}
-                            onChange={(e) => setTimeline(e.target.value)}
-                            disabled={submitted}
-                        />
-                    </div>
-                    <div>
-                        <button onClick={handleSubmit}>Submit</button>
-                        {!submitted && error && <p style={{ color: 'red' }}>{error}</p>}
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <h2>YouTube Audio Spleeter</h2>
+      <input
+        className="input-field"
+        type="text"
+        value={link}
+        onChange={handleLinkChange}
+        placeholder="YouTube 링크"
+      />
+      <input
+        className="input-field"
+        type="text"
+        value={start}
+        onChange={handleStartChange}
+        placeholder="시작 시간 (초)"
+      />
+      <input
+        className="input-field"
+        type="text"
+        value={end}
+        onChange={handleEndChange}
+        placeholder="종료 시간 (초)"
+      />
+      <button className="submit-button" onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
 
-export default YouTubeInputComponent;
+export default YouTubeComponent;
